7c89a6d592dbddc83420cc94f4574473041790bb
hanyangxn <m18110024862@163.com>
2020-08-28 11:29:37 +0800

    Initial commit

---
+++ src/test/java/com/hy/sms_demo/SmsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hy.sms_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
